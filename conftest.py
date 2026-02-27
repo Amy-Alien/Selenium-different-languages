a28ad6eb551fdc27218082dc4c5b843ca79a4340
amyalien <hughsence@gmail.com>
2026-02-27 22:32:09 +0400

    Added conftest file with choice of browser and language

---
+++ conftest.py
@@ -0,0 +1,36 @@
+import time
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser_name", action="store",
+                     default="chrome", help="Choose browser: chrome or firefox")
+    parser.addoption("--language", action="store", default="es", help="Choose language")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("--browser_name")
+    language = request.config.getoption("--language")
+    if browser_name == "chrome":
+        options = ChromeOptions()
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+
+        print("--- Starting Chrome browser ---")
+        browser = webdriver.Chrome(options=options)
+
+    elif browser_name == "firefox":
+        options = FirefoxOptions()
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+
+        print("--- Starting Firefox browser ---")
+        browser = webdriver.Firefox(options=options)
+
+    yield browser
+    print("--- Quit browser ---")
+    time.sleep(2)
+    browser.quit()
+
+
